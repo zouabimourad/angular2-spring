318fcca918ebca47dea4623a0cb7abe7499ed30d
mouradzouabi <zouabi.mourad@gmail.com>
2016-03-05 20:04:42 +0100

    don't suspend server when debugging

--- server/run.sh
+++ server/run.sh
@@ -1,3 +1,3 @@
 
-export MAVEN_OPTS="-Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000"
+export MAVEN_OPTS="-Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000"
 mvn jetty:run -Dspring.profiles.active=swagger-ui
